cc054a22d99054a9d763dd1c5f90e8aecfc3f473
arekpytka2812 <arek.pytka2812@gmail.com>
2022-10-25 17:10:04 +0200

    kamera

---
+++ UP1/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace UP1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxDevices = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.buttonStartRecording = new System.Windows.Forms.Button();
+            this.buttonStopRecording = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelRecordingStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxDevices
+            // 
+            this.comboBoxDevices.FormattingEnabled = true;
+            this.comboBoxDevices.Location = new System.Drawing.Point(13, 14);
+            this.comboBoxDevices.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBoxDevices.Name = "comboBoxDevices";
+            this.comboBoxDevices.Size = new System.Drawing.Size(800, 24);
+            this.comboBoxDevices.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 44);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 415);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(820, 12);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(240, 25);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(820, 107);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(240, 26);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Save Frame";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(820, 44);
+            this.buttonStop.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(240, 28);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(820, 329);
+            this.trackBar2.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar2.Minimum = -10;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(240, 56);
+            this.trackBar2.TabIndex = 8;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(903, 368);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Saturation";
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(823, 393);
+            this.trackBar3.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar3.Maximum = 255;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(240, 56);
+            this.trackBar3.TabIndex = 10;
+            this.trackBar3.Value = 180;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // buttonStartRecording
+            // 
+            this.buttonStartRecording.Location = new System.Drawing.Point(820, 175);
+            this.buttonStartRecording.Name = "buttonStartRecording";
+            this.buttonStartRecording.Size = new System.Drawing.Size(237, 31);
+            this.buttonStartRecording.TabIndex = 12;
+            this.buttonStartRecording.Text = "Start Recording";
+            this.buttonStartRecording.UseVisualStyleBackColor = true;
+            this.buttonStartRecording.Click += new System.EventHandler(this.buttonStartRecording_Click);
+            // 
+            // buttonStopRecording
+            // 
+            this.buttonStopRecording.Location = new System.Drawing.Point(820, 212);
+            this.buttonStopRecording.Name = "buttonStopRecording";
+            this.buttonStopRecording.Size = new System.Drawing.Size(237, 32);
+            this.buttonStopRecording.TabIndex = 13;
+            this.buttonStopRecording.Text = "Stop Recording";
+            this.buttonStopRecording.UseVisualStyleBackColor = true;
+            this.buttonStopRecording.Click += new System.EventHandler(this.buttonStopRecording_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(925, 432);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 17);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Hue";
+            // 
+            // labelRecordingStatus
+            // 
+            this.labelRecordingStatus.AutoSize = true;
+            this.labelRecordingStatus.Location = new System.Drawing.Point(886, 258);
+            this.labelRecordingStatus.Name = "labelRecordingStatus";
+            this.labelRecordingStatus.Size = new System.Drawing.Size(0, 17);
+            this.labelRecordingStatus.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1076, 479);
+            this.Controls.Add(this.labelRecordingStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonStopRecording);
+            this.Controls.Add(this.buttonStartRecording);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBoxDevices);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxDevices;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Button buttonStartRecording;
+        private System.Windows.Forms.Button buttonStopRecording;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelRecordingStatus;
+    }
+}
+
---
+++ UP1/Form1.cs
@@ -0,0 +1,179 @@
+﻿using AForge.Video.DirectShow;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using AForge.Video;
+using Accord.Video.FFMPEG;
+using System.IO;
+using AForge.Vision.Motion;
+using AForge.Imaging.Filters;
+using AForge.Math.Random;
+
+namespace UP1
+{
+    public partial class Form1 : Form
+    {
+        FilterInfoCollection videoDeviceList;
+        VideoCaptureDevice capturedDevice;
+        VideoFileWriter videoFileWriter;
+        Bitmap frame;
+        Double zoom = 0.5;
+
+        HueModifier hueFilter = new HueModifier(180);
+        SaturationCorrection satFilter = new SaturationCorrection(0.5f);
+
+        bool isCameraOn = false;
+        bool isRecording = false;
+
+        int photoCount = 1;
+        int filmCount = 1;
+
+        string defaultPhotoPath = @"C:/Users/arpy2/Desktop/zdjecie";
+        string defaultFilmPath = @"C:/Users/arpy2/Desktop/video";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // creating devices list and adding them to combobox
+            videoDeviceList = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach (FilterInfo videoDevice in videoDeviceList)
+            {
+                comboBoxDevices.Items.Add(videoDevice.Name);
+            }
+
+            // creating new capture device
+            capturedDevice = new VideoCaptureDevice();
+
+            // prevents user from not choosing any device
+            comboBoxDevices.SelectedIndex = 0;
+
+            // changing flags and buttons' statuses
+            buttonStop.Enabled = false;
+            buttonStartRecording.Enabled = false;
+            buttonStopRecording.Enabled = false;
+            buttonSave.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // getting chosen device and starting signal
+            capturedDevice = new VideoCaptureDevice(videoDeviceList[comboBoxDevices.SelectedIndex].MonikerString);
+            capturedDevice.NewFrame += new NewFrameEventHandler(video_NewFrame);
+            capturedDevice.Start();
+
+            // changing flags and buttons' statuses
+            isCameraOn = true;
+            buttonStart.Enabled = false;
+            buttonStop.Enabled = true;
+            buttonStartRecording.Enabled = true;
+            buttonSave.Enabled = true;
+        }
+
+        private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            // cloning frame and applying various filters
+            frame = (Bitmap)eventArgs.Frame.Clone();
+            frame = new Bitmap(frame, new Size((int)(frame.Width * zoom), (int)(frame.Height * zoom)));
+            frame = hueFilter.Apply(frame);
+            frame = satFilter.Apply(frame);
+            pictureBox1.Image = frame;
+
+            // saving frame for recorded film
+            if (isRecording)
+                videoFileWriter.WriteVideoFrame((Bitmap)frame.Clone());  
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            // stoping signal
+            capturedDevice.SignalToStop();
+            
+            // "clearing" picturebox
+            pictureBox1.Image = null;
+
+            // changing flags and buttons' statuses
+            isCameraOn = false;
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+            buttonSave.Enabled = false;
+            buttonStartRecording.Enabled = false;
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            // creating path for saved frame
+            var path = defaultPhotoPath + photoCount.ToString() + ".bmp";
+            photoCount++;
+
+            // deleting if path exists
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            // cloning saved frame
+            Bitmap imageToSave = (Bitmap)pictureBox1.Image.Clone();
+
+            // saving frame
+            imageToSave.Save(path);
+        }
+
+        private void buttonStartRecording_Click(object sender, EventArgs e)
+        {
+            // creating path for recorded film
+            var path = defaultFilmPath + filmCount.ToString() + ".avi";
+            filmCount++;
+
+            // deleting if path exists
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            // creating VideoFileWriter instance
+            videoFileWriter = new VideoFileWriter();
+
+            // opening stream
+            videoFileWriter.Open(path, pictureBox1.Image.Width, pictureBox1.Image.Height, 13, VideoCodec.MPEG4);
+
+            // changing flags and buttons' statuses
+            isRecording = true;
+            buttonStartRecording.Enabled = false;
+            buttonStopRecording.Enabled = true;
+
+            // changing recordingLabel text
+            labelRecordingStatus.Text = "Recording!";
+        }
+
+        private void buttonStopRecording_Click(object sender, EventArgs e)
+        {
+            // closing VideoFileWriter stream
+            videoFileWriter.Close();
+
+            // changing flags and buttons' statuses
+            buttonStartRecording.Enabled = true;
+            buttonStopRecording.Enabled = false;
+            isRecording = false;
+
+            // setting recordingStatusLabel to null
+            labelRecordingStatus.Text = "";
+        }
+
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            // setting up saturation filter value
+            satFilter.AdjustValue = (float)trackBar2.Value / 10;
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            // setting up hue filter value
+            hueFilter.Hue = trackBar3.Value;
+        }
+
+        
+    }
+}
